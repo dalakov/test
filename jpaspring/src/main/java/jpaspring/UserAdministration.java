0496aa1fd9ed9d1e07614e1472ad810d3d16a8b0
dalakov <dalakov.mail@gmail.com>
2017-11-14 03:02:42 +0200

    !

--- jpaspring/src/main/java/jpaspring/UserAdministration.java
+++ jpaspring/src/main/java/jpaspring/UserAdministration.java
@@ -1,14 +1,5 @@
 package jpaspring;
 
-import jpaspring.entity.AppUser;/*
-import org.lightadmin.api.config.AdministrationConfiguration;
-import org.lightadmin.api.config.builder.EntityMetadataConfigurationUnitBuilder;
-import org.lightadmin.api.config.builder.FieldSetConfigurationUnitBuilder;
-import org.lightadmin.api.config.builder.ScreenContextConfigurationUnitBuilder;
-import org.lightadmin.api.config.unit.EntityMetadataConfigurationUnit;
-import org.lightadmin.api.config.unit.FieldSetConfigurationUnit;
-import org.lightadmin.api.config.unit.ScreenContextConfigurationUnit;
-*/
 public class UserAdministration
         //extends AdministrationConfiguration<AppUser>
         {
